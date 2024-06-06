e90393bd6788bbb76f65e31a9b92f750a82e2b7b
Thevid Oswald Berrius <thevidob@gmail.com>
2024-06-06 19:51:34 +0700

    Edited to gridview transaction history detail

--- LabProjectRAiso/Views/Pages/Customer/TransactionDetail.aspx.cs
+++ LabProjectRAiso/Views/Pages/Customer/TransactionDetail.aspx.cs
@@ -1,6 +1,4 @@
-﻿using LabProjectRAiso.Controller;
-using LabProjectRAiso.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,36 +11,7 @@ namespace LabProjectRAiso.Views.Pages.Customer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(!IsPostBack)
-            {
-                if (Session["user"] != null)
-                {
-                    String role = UserController.GetUserRole(Session["user"].ToString());
-                    if (!role.Equals("Customer")) Response.Redirect("~/Views/Pages/Home.aspx");
-                    else
-                    {
-                        if (Request["id"] == null) Response.Redirect("~/Views/Pages/Home.aspx");
-                        else
-                        {
-                            int userID = Convert.ToInt32(Session["user"]);
-                            int transactionID = Convert.ToInt32(Request["id"]);
-                            TransactionHeader header = TransactionController.GetHeader(userID, transactionID);
 
-                            if (header == null)
-                            {
-                                Response.Redirect("~/Views/Pages/Home.aspx");
-                            }
-
-                            LabProjectRAiso.Model.TransactionDetail modelDetail = TransactionController.GetDetail(transactionID);
-                            TBox_Name.Text = modelDetail.MsStationery.StationeryName;
-                            TBox_Price.Text = modelDetail.MsStationery.StationeryPrice.ToString();
-                            TBox_Quantity.Text = modelDetail.Quantity.ToString();
-
-                        }
-                    }
-                }
-                else Response.Redirect("~/Views/Pages/Home.aspx");
-            }
         }
     }
 }
